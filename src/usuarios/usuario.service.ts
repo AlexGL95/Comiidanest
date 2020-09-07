e23f7f03e1096994089af4a97bbf2ae59bc16822
Alex <glopezalex631@gmail.com>
2020-09-07 18:24:49 -0500

    Peticiones usuarios
    todas las peticiones sin probar

--- src/usuarios/usuario.service.ts
+++ src/usuarios/usuario.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Usuarios } from './usuarios';
 import { Repository } from 'typeorm';
+import { CreateUsuariodto } from './dto/create-usuariodto';
 
 @Injectable()
 export class UsuarioService {
@@ -11,8 +12,31 @@ export class UsuarioService {
         private readonly userRepository: Repository<Usuarios>
     ){} 
 
-    async getAll() {
+    async getAll():Promise<Usuarios[]> {
         return await this.userRepository.find();
+    }
+
+    async createUser(newuser: CreateUsuariodto):Promise<Usuarios>{
+        const nuevo = new Usuarios();
+        nuevo.id=0;
+        nuevo.nombre=newuser.nombre;
+        nuevo.pass=newuser.pass;
+        nuevo.equipo=newuser.equipoid;
+
+        return this.userRepository.save(nuevo)
+    }
+
+    async updateUsuario(idUsuario:number, usuarioActualizar: CreateUsuariodto):Promise<Usuarios>{
+        const usuarioupdate = await this.userRepository.findOne(idUsuario);
+        usuarioupdate.nombre=usuarioActualizar.nombre;
+        usuarioupdate.pass=usuarioActualizar.pass;
+        usuarioupdate.equipo=usuarioActualizar.equipoid;
+
+        return await this.userRepository.save(usuarioupdate)
+    }
+
+    async deleteusuario(idmensaje:number):Promise <any>{
+        return await this.userRepository.delete(idmensaje);
     }
 
 
