b6a74bdeb0c52aa63f0005847a1fae747f254d7f
asdasd <asdasd>
2020-09-15 18:27:33 -0500

    Fix1

--- src/equipo/equipo.service.ts
+++ src/equipo/equipo.service.ts
@@ -47,11 +47,11 @@ export class EquiposService {
         }
 
         for (let k = 0; k < (Math.floor(array.length/2)); k++) {
-                let d1 = moment(foundTeam[foundTeam.length-1].fecha).add(k+1+s, 'days').weekday();
+                let d1 = moment().add(k+1+s, 'days').weekday();
                 if(d1===6){
                     s = s+2;
                 }
-                let d4 = moment(foundTeam[foundTeam.length-1].fecha).add(k+1+s, 'days').toDate();
+                let d4 = moment().add(k+1+s, 'days').toDate();
                 
                 
                 
