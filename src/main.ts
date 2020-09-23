90ebb40a0d0a8f594167cc4c0f07d01924e85e3f
asdasd <asdasd>
2020-09-23 12:35:51 -0500

    CORS añadido para localhost:4200

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const CORS_OPTIONS = {
+  "origin": "http://localhost:4200",
+  "methods": "GET,HEAD,PUT,POST,DELETE",
+  "allowedHeaders": 'Content-Type',
+  "preflightContinue": true,
+  "optionsSuccessStatus": 204
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: CORS_OPTIONS });
   await app.listen(3000);
 }
 bootstrap();
